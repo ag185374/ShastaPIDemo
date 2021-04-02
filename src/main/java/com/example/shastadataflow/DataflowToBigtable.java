f73411f07e221713e4ec07b25b5ab3bc78def70d
Tang <nt185083@ncr.com>
2021-04-02 16:27:23 -0400

    implement, add totalCount column in bigtable row

--- src/main/java/com/example/shastadataflow/DataflowToBigtable.java
+++ src/main/java/com/example/shastadataflow/DataflowToBigtable.java
@@ -7,6 +7,7 @@ import com.google.bigtable.repackaged.com.google.cloud.bigtable.admin.v2.Bigtabl
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.BigtableDataClient;
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.models.Filters;
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.models.Row;
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.models.RowCell;
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.models.RowMutation;
@@ -45,41 +46,41 @@ public class DataflowToBigtable {
     private static String outputTopic = "projects/ret-shasta-cug01-dev/topics/Shasta-PI-outbound";
     private static BigtableDataClient dataClient;
     private static BigtableTableAdminClient adminClient;
-    private static String tableId = "dataflow";
+    private static String tableId = "pi-dataflow-inventory";
 
 
     public static void main(String[] args) throws IOException {
         // [START apache_beam_create_pipeline]
-        BigtableOptions bigtableOptions = PipelineOptionsFactory.create().as(BigtableOptions.class);
+//        BigtableOptions bigtableOptions = PipelineOptionsFactory.create().as(BigtableOptions.class);
         PipelineOptions pipelineOptions = PipelineOptionsFactory.fromArgs(args).withValidation().as(PipelineOptions.class);
         Pipeline pipeline = Pipeline.create(pipelineOptions);
         // [END apache_beam_create_pipeline]
 
 
-        // Creates the settings to configure a bigtable data client.
-        BigtableDataSettings settings =
-                BigtableDataSettings.newBuilder().setProjectId(bigtableOptions.getBigtableProjectId())
-                        .setInstanceId(bigtableOptions.getBigtableInstanceId()).build();
-        // Creates a bigtable data client.
-        dataClient = BigtableDataClient.create(settings);
-        // Creates the settings to configure a bigtable table admin client.
-                BigtableTableAdminSettings adminSettings =
-                        BigtableTableAdminSettings.newBuilder()
-                                .setProjectId(bigtableOptions.getBigtableProjectId())
-                                .setInstanceId(bigtableOptions.getBigtableInstanceId())
-                                .build();
-
-        // Creates a bigtable table admin client.
-        adminClient = BigtableTableAdminClient.create(adminSettings);
-
-        // [START bigtable_beam_helloworld_write_config]
-        CloudBigtableTableConfiguration bigtableTableConfig =
-                new CloudBigtableTableConfiguration.Builder()
-                        .withProjectId(bigtableOptions.getBigtableProjectId())
-                        .withInstanceId(bigtableOptions.getBigtableInstanceId())
-                        .withTableId(bigtableOptions.getBigtableTableId())
-                        .build();
-
+//        // Creates the settings to configure a bigtable data client.
+//        BigtableDataSettings settings =
+//                BigtableDataSettings.newBuilder().setProjectId(bigtableOptions.getBigtableProjectId())
+//                        .setInstanceId(bigtableOptions.getBigtableInstanceId()).build();
+//        // Creates a bigtable data client.
+//        dataClient = BigtableDataClient.create(settings);
+//        // Creates the settings to configure a bigtable table admin client.
+//                BigtableTableAdminSettings adminSettings =
+//                        BigtableTableAdminSettings.newBuilder()
+//                                .setProjectId(bigtableOptions.getBigtableProjectId())
+//                                .setInstanceId(bigtableOptions.getBigtableInstanceId())
+//                                .build();
+//
+//        // Creates a bigtable table admin client.
+//        adminClient = BigtableTableAdminClient.create(adminSettings);
+//
+//        // [START bigtable_beam_helloworld_write_config]
+//        CloudBigtableTableConfiguration bigtableTableConfig =
+//                new CloudBigtableTableConfiguration.Builder()
+//                        .withProjectId(bigtableOptions.getBigtableProjectId())
+//                        .withInstanceId(bigtableOptions.getBigtableInstanceId())
+//                        .withTableId(bigtableOptions.getBigtableTableId())
+//                        .build();
+//
 
         /*
          * Step #1: Read messages in from Pub/Sub Subscription
@@ -92,16 +93,16 @@ public class DataflowToBigtable {
                                 .fromSubscription(inputSubscription));
 
         /*
-         * Step #2: Write Pubsub messages into bigtable
+         * Step #2: Write Pubsub messages and total count into bigtable
          */
-        PCollection<String> rowKeys =
+        PCollection<String> inventorySum =
                 messages.apply("WritePubsubToBigtable", ParDo.of(new PubsubToBigtable()));
 
-        /*
-         * Step #3: Calculate the count sum for written rows
-         */
-        PCollection<String>  inventorySum=
-                rowKeys.apply("CalculateSumFromBigtable", ParDo.of(new CalculateSumFromBigtable()));
+//        /*
+//         * Step #3: Calculate the count sum for written rows
+//         */
+//        PCollection<String>  inventorySum=
+//                rowKeys.apply("CalculateSumFromBigtable", ParDo.of(new CalculateSumFromBigtable()));
 
         /*
          * Step #4: Publish the message to Pubsub
@@ -125,27 +126,52 @@ public class DataflowToBigtable {
 
     }
 
-    public interface BigtableOptions extends DataflowPipelineOptions {
-        @Description("The Bigtable project ID, this can be different than your Dataflow project")
-        @Default.String("ret-shasta-cug01-dev")
-        String getBigtableProjectId();
-
-        void setBigtableProjectId(String bigtableProjectId);
-
-        @Description("The Bigtable instance ID")
-        @Default.String("pi-bigtable")
-        String getBigtableInstanceId();
-
-        void setBigtableInstanceId(String bigtableInstanceId);
-
-        @Description("The Bigtable table ID in the instance.")
-        @Default.String("pi-dataflow-inventory")
-        String getBigtableTableId();
-
-        void setBigtableTableId(String bigtableTableId);
-    }
+//    public interface BigtableOptions extends DataflowPipelineOptions {
+//        @Description("The Bigtable project ID, this can be different than your Dataflow project")
+//        @Default.String("ret-shasta-cug01-dev")
+//        String getBigtableProjectId();
+//
+//        void setBigtableProjectId(String bigtableProjectId);
+//
+//        @Description("The Bigtable instance ID")
+//        @Default.String("pi-bigtable")
+//        String getBigtableInstanceId();
+//
+//        void setBigtableInstanceId(String bigtableInstanceId);
+//
+//        @Description("The Bigtable table ID in the instance.")
+//        @Default.String("pi-dataflow-inventory")
+//        String getBigtableTableId();
+//
+//        void setBigtableTableId(String bigtableTableId);
+//    }
 
     static class PubsubToBigtable extends DoFn<PubsubMessage, String> {
+        private static BigtableDataClient dataClient;
+        private static BigtableTableAdminClient adminClient;
+        private static String tableId = "pi-dataflow-inventory";
+        private static String bigtableProjectId = "ret-shasta-cug01-dev";
+        private static String bigtableInstanceId = "pi-bigtable";
+
+        @Setup
+        public void initializeBigtableConnection() throws IOException {
+            // Creates the settings to configure a bigtable data client.
+            BigtableDataSettings settings =
+                    BigtableDataSettings.newBuilder().setProjectId(bigtableProjectId)
+                            .setInstanceId(bigtableInstanceId).build();
+            // Creates a bigtable data client.
+            dataClient = BigtableDataClient.create(settings);
+            // Creates the settings to configure a bigtable table admin client.
+            BigtableTableAdminSettings adminSettings =
+                    BigtableTableAdminSettings.newBuilder()
+                            .setProjectId(bigtableProjectId)
+                            .setInstanceId(bigtableInstanceId)
+                            .build();
+
+            // Creates a bigtable table admin client.
+            adminClient = BigtableTableAdminClient.create(adminSettings);
+        }
+
         @ProcessElement
         public void processElement(@Element PubsubMessage message, OutputReceiver<String> out) throws JsonProcessingException {
             String payload = new String(message.getPayload(), StandardCharsets.UTF_8);
@@ -153,13 +179,30 @@ public class DataflowToBigtable {
             Inventory inventory = mapper.readValue(payload, Inventory.class);
             String rowKey = "Dataflow#Count#Dept#"+inventory.documentId+"#UPC#"+inventory.UPC+"#ItemCode#"+inventory.itemCode;
 
+            Filters.Filter filter = Filters.FILTERS.limit().cellsPerColumn(1);
+
+            Row btRow = dataClient.readRow(tableId, rowKey,filter);
+            System.out.println("rowKey ******************** " + rowKey);
+            int totalCount = Integer.parseInt(inventory.count);
+            if (btRow != null){
+                List<RowCell> cell  = btRow.getCells("cf-meta","totalCount");
+                if (cell.size() != 0){
+                    totalCount += Integer.parseInt(cell.get(0).getValue().toStringUtf8());
+                    System.out.println("timeStamp ******************** " + cell.get(0).getTimestamp());
+                }
+            }
             RowMutation rowMutation =
                     RowMutation.create(tableId, rowKey)
                             .setCell("cf-meta", "count",inventory.count)
+                            .setCell("cf-meta","totalCount",String.valueOf(totalCount))
                             .setCell("cf-meta", "payload", payload)
                             .setCell("cf-meta", "createdDate", inventory.effectiveDate);
             dataClient.mutateRow(rowMutation);
-            out.output(rowKey);
+
+            // Construct pubsub message
+            InventorySum inventorySum = new InventorySum(inventory.itemCode,inventory.UPC,inventory.documentId,String.valueOf(totalCount));
+            String json = mapper.writeValueAsString(inventorySum);
+            out.output(json);
 
             // Use Bigtable IO connector
 //            Put row = new Put(Bytes.toBytes(rowKey));
@@ -170,27 +213,42 @@ public class DataflowToBigtable {
     }
 
 
-    static class CalculateSumFromBigtable extends DoFn<String, String> {
+    static class ReadSumFromBigtable extends DoFn<String, String> {
+        private static BigtableDataClient dataClient;
+        private static BigtableTableAdminClient adminClient;
+        private static String tableId = "pi-dataflow-inventory";
+        private static String bigtableProjectId = "ret-shasta-cug01-dev";
+        private static String bigtableInstanceId = "pi-bigtable";
+
+        @Setup
+        public void initializeBigtableConnection() throws IOException {
+            // Creates the settings to configure a bigtable data client.
+            BigtableDataSettings settings =
+                    BigtableDataSettings.newBuilder().setProjectId(bigtableProjectId)
+                            .setInstanceId(bigtableInstanceId).build();
+            // Creates a bigtable data client.
+            dataClient = BigtableDataClient.create(settings);
+            // Creates the settings to configure a bigtable table admin client.
+            BigtableTableAdminSettings adminSettings =
+                    BigtableTableAdminSettings.newBuilder()
+                            .setProjectId(bigtableProjectId)
+                            .setInstanceId(bigtableInstanceId)
+                            .build();
+
+            // Creates a bigtable table admin client.
+            adminClient = BigtableTableAdminClient.create(adminSettings);
+        }
+
         @ProcessElement
         public void processElement(@Element String rowKey, OutputReceiver<String> out) throws JsonProcessingException {
             Row btRow = dataClient.readRow(tableId, rowKey);
             System.out.println("rowKey ******************** " + rowKey);
-            int totalCount = 0;
-            if (btRow != null){
-                List<RowCell> cells  = btRow.getCells("cf-meta","count");
-                for (RowCell cell : cells) {
-                    int curCount = Integer.parseInt(cell.getValue().toStringUtf8());
-                    System.out.println("curCount ******************** " + curCount);
-                    System.out.println("timeStamp ******************** " + cell.getTimestamp());
-                    totalCount += curCount;
-                }
-            }
 
             String[] itemKeys = rowKey.split("#");
             String documentId = itemKeys[3];
             String UPC = itemKeys[5];
             String itemCode = itemKeys[7];
-            InventorySum inventorySum = new InventorySum(itemCode,UPC,documentId,String.valueOf(totalCount));
+            InventorySum inventorySum = new InventorySum(itemCode,UPC,documentId,String.valueOf(1));
             ObjectMapper mapper = new ObjectMapper();
             String json = mapper.writeValueAsString(inventorySum);
             out.output(json);
